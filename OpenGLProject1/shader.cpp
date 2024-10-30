030b45097f4bde99e300cf27e33f0426399e3240
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2024-10-30 19:59:59 +0200

    made rotating camera around a point

--- OpenGLProject1/shader.cpp
+++ OpenGLProject1/shader.cpp
@@ -244,21 +244,26 @@ int main()
 
         ourShader.use();
      
-        glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
    
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f); 
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+      
 
-		glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);//camera position
-		glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 00.0f);//camera target
-		glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);//camera direction
+		glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+		glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 00.0f);
+		glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
 
         glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
 		glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
 
         glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
 
+	
+
+        const float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		glm::mat4 view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
  
 		
         ourShader.setMat4("projection", projection);
