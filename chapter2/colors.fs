389d1640f1749c424bee2b342faceef38dc245fd
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2024-10-31 19:19:32 +0200

    phong lighting made with ambient, diffuse and specular lighting

--- chapter2/colors.fs
+++ chapter2/colors.fs
@@ -9,7 +9,9 @@ in vec3 FragPos;
 uniform vec3 lightPos; 
 uniform vec3 lightColor;
 uniform vec3 objectColor;
+uniform vec3 viewPos;
 
+float specularStrength = 0.5;
 void main()
 {
     // ambient
@@ -21,8 +23,14 @@ void main()
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
+
+    //specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;  
             
-    vec3 result = (ambient + diffuse) * objectColor;
-    FragColor = vec4(result, 1.0);
+vec3 result = (ambient + diffuse + specular) * objectColor;
+FragColor = vec4(result, 1.0);
 } 
 
--- chapter2/window.cpp
+++ chapter2/window.cpp
@@ -173,6 +173,8 @@ int main()
 		lightingShader.setVec3("objectColor", coral);	
 		lightingShader.setVec3("lightColor", lightColor);
 		lightingShader.setVec3("lightPos", lightPos);
+		lightingShader.setVec3("viewPos", camera.Position);
+
 
 
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
