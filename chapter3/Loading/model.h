d0a7c5e975d35dd16be375439e31d8f354670277
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2024-11-12 22:51:24 +0200

    how the fuck do I make the model work as well as the mesh class jeez

---
+++ chapter3/Loading/model.h
@@ -0,0 +1,35 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <string>
+
+#include "shader.h"
+#include "camera.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <iostream>
+
+
+class Model
+{
+public:
+    Model(char* path)
+    {
+        loadModel(path);
+    }
+    void Draw(Shader& shader);
+private:
+    // model data
+    vector<Mesh> meshes;
+    string directory;
+
+    void loadModel(string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+        string typeName);
+};
