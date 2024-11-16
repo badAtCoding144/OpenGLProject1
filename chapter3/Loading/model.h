f54d95181eada0b2c0d65a6d1a320f5df92c36bf
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2024-11-16 16:54:36 +0200

    figure out the correct headers for texturefrom file and also figure out why the declaration of the diffuse and specular map vectrors is breaking like dis

--- chapter3/Loading/model.h
+++ chapter3/Loading/model.h
@@ -15,6 +15,7 @@
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
+#include "mesh.h"
 
 class Model
 {
@@ -30,6 +31,104 @@ public:
             meshes[i].Draw(shader);
     }
 
+    void loadModel(string path)
+    {
+        Assimp::Importer import;
+        const aiScene * scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+        {
+            cout << "ERROR::ASSIMP::" << import.GetErrorString() << endl;
+            return;
+        }
+        directory = path.substr(0, path.find_last_of('/'));
+
+        processNode(scene->mRootNode, scene);
+    }
+
+    void processNode(aiNode* node, const aiScene* scene)
+    {
+        // process all the node's meshes (if any)
+        for (unsigned int i = 0; i < node->mNumMeshes; i++)
+        {
+            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+            meshes.push_back(processMesh(mesh, scene));
+        }
+        // then do the same for each of its children
+        for (unsigned int i = 0; i < node->mNumChildren; i++)
+        {
+            processNode(node->mChildren[i], scene);
+        }
+    }
+
+    Mesh processMesh(aiMesh* mesh, const aiScene *scene) 
+    {
+        vector<Vertex> vertices; 
+		vector<unsigned int> indices;
+        vector <Texture> textures;
+
+
+            for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+            {
+				Vertex vertex;
+				glm::vec3 vector;
+				// positions
+				vector.x = mesh->mVertices[i].x;
+				vector.y = mesh->mVertices[i].y;
+				vector.z = mesh->mVertices[i].z;
+				vertex.Position = vector;
+				// normals
+				vector.x = mesh->mNormals[i].x;
+				vector.y = mesh->mNormals[i].y;
+				vector.z = mesh->mNormals[i].z;
+				vertex.Normal = vector;
+				// texture coordinates
+				if (mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
+				{
+					glm::vec2 vec;
+					// a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't 
+					// use models where a vertex can have multiple texture coordinates so we always take the first set (0).
+					vec.x = mesh->mTextureCoords[0][i].x;
+					vec.y = mesh->mTextureCoords[0][i].y;
+					vertex.TexCoords = vec;
+				}
+				else
+					vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+
+                for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+                {
+                    aiFace face = mesh->mFaces[i];
+                    for (unsigned int j = 0; j < face.mNumIndices; j++)
+                        indices.push_back(face.mIndices[j]);
+                }
+                if (mesh->mMaterialIndex >= 0)
+                {
+                    aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+                    vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+                    textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+                    vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+                    textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+                }
+				vertices.push_back(vertex);
+            }
+    }
+
+
+    vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName)
+    {
+        vector<Texture> textures;
+        for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+        {
+            aiString str;
+            mat->GetTexture(type, i, &str);
+            Texture texture;
+            texture.id = TextureFromFile(str.C_Str(), directory);
+            texture.type = typeName;
+            texture.path = str;
+            textures.push_back(texture);
+        }
+        return textures;
+    }
 
 private:
     // model data
