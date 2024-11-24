07065031411c8ca70bdbaa44acf4db16b2b635e0
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2024-11-24 21:40:49 +0200

    we made the linear depth values for how far we are away from the far frustum to be light and dark when close to the near plane of the projection frustum

--- DepthTesting/DepthTesting/window.cpp
+++ DepthTesting/DepthTesting/window.cpp
@@ -39,16 +39,14 @@ float lastFrame = 0.0f;
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
+
+    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+
 
     // glfw window creation
     // --------------------
