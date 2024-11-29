0f135eae6e4b11a14b954024bcc234160fadceb5
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2024-11-29 19:23:17 +0200

    did the same with instancing but the asteroids are not drawing, will check it out now but probably something outside the render loop

--- DepthTesting/DepthTesting/InstancingEx3D.cpp
+++ DepthTesting/DepthTesting/InstancingEx3D.cpp
@@ -79,6 +79,7 @@ int main()
     // build and compile shaders
 
     Shader shader("Asteroid.vs", "Asteroid.fs");
+	Shader planetShader("planet.vs", "planet.fs");
 
 	const char*  rockPath = "resources/objects/rock/rock.obj";
 	const char* planetPath = "resources/objects/planet/planet.obj";
@@ -124,9 +125,32 @@ int main()
 
 
 
+    unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer); 
+	glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW);
 
+    for (unsigned int i = 0; i < rock.meshes.size(); i++)
+    {
+        unsigned int VAO = rock.meshes[i].VAO;
+        glBindVertexArray(VAO);
+        // set attribute pointers for matrix (4 times vec4)
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)));
+        glEnableVertexAttribArray(5);
+        glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4)));
+        glEnableVertexAttribArray(6);
+        glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4)));
 
+        glVertexAttribDivisor(3, 1);
+        glVertexAttribDivisor(4, 1);
+        glVertexAttribDivisor(5, 1);
+        glVertexAttribDivisor(6, 1);
 
+        glBindVertexArray(0);
+    }
 
 
 
@@ -150,30 +174,37 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
+        // configure transformation matrices
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 1000.0f);
         glm::mat4 view = camera.GetViewMatrix();
-
         shader.use();
-		shader.setMat4("projection", projection);
-		shader.setMat4("view", view);
+        shader.setMat4("projection", projection);
+        shader.setMat4("view", view);//shader is for asteroids and planetshader for planet we just set the matrices
+        planetShader.use();
+        planetShader.setMat4("projection", projection);
+        planetShader.setMat4("view", view);
 
+        // draw planet
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
         model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
-        shader.setMat4("model", model);
-        planet.Draw(shader);
+        planetShader.setMat4("model", model);
+        planet.Draw(planetShader);
 
-        // draw meteorites
-        for (unsigned int i = 0; i < amount; i++)
+		// draw meteorites
+                // draw meteorites
+        shader.use();
+        shader.setInt("texture_diffuse1", 0);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, rock.textures_loaded[0].id); // note: we also made the textures_loaded vector public (instead of private) from the model class.
+        for (unsigned int i = 0; i < rock.meshes.size(); i++)
         {
-            shader.setMat4("model", modelMatrices[i]);
-            rock.Draw(shader);
+            glBindVertexArray(rock.meshes[i].VAO);
+            glDrawElementsInstanced(GL_TRIANGLES, static_cast<unsigned int>(rock.meshes[i].indices.size()), GL_UNSIGNED_INT, 0, amount);
+            glBindVertexArray(0);
         }
 
-
-
-
-
+        
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
