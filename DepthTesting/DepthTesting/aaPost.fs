530167e23e15cc47388158b32627f394564f2719
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2024-11-30 01:03:18 +0200

    virginity is cool cmon cmon

--- DepthTesting/DepthTesting/aaPost.fs
+++ DepthTesting/DepthTesting/aaPost.fs
@@ -9,8 +9,8 @@ uniform sampler2D screenTexture;
 
 void main()
 {
-    vec3 col = texture(screenTexture, TexCoords).rgb;
-    float grayscale = 0.2126 * col.r + 0.7152 * col.g + 0.0722 * col.b;
+    //vec3 col = texture(screenTexture, TexCoords).rgb;
+    //float grayscale = 0.2126 * col.r + 0.7152 * col.g + 0.0722 * col.b;
     FragColor = vec4(vec3(texture(screenTexture, TexCoords).rgb), 1.0);
 } 
 
