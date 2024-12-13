266ebcd3c750014c04475008edd5a859157e1bc9
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2024-12-13 18:15:52 +0200

    well I did something but I need to look at this code again

--- PBR/PBR.cpp
+++ PBR/PBR.cpp
@@ -109,7 +109,7 @@ int main()
         glm::vec3(300.0f, 300.0f, 300.0f)
     };
 	int nrRows = 7;
-    int nrColumsn = 7;
+    int nrColumns = 7;
     float spacing = 2.5;
 
 	glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -124,69 +124,68 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         // per-frame time logic
-
-        auto currentFrame = static_cast<float>(glfwGetTime());
+        // --------------------
+        float currentFrame = static_cast<float>(glfwGetTime());
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
         // input
-
+        // -----
         processInput(window);
 
         // render
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        // ------
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 
         shader.use();
         glm::mat4 view = camera.GetViewMatrix();
-        shader.setVec3("campos", camera.Position);
-		shader.setMat4("view", view);
+        shader.setMat4("view", view);
+        shader.setVec3("camPos", camera.Position);
 
         // render rows*column number of spheres with varying metallic/roughness values scaled by rows and columns respectively
-		glm::mat4 model = glm::mat4(1.0f);
-
-        for (int row = 0; row < nrRows; row++) {
-        
-			shader.setFloat("metallic", (float)row / (float)nrRows);
-			for (int col = 0; col < nrColumsn; col++) {
-
-
+        glm::mat4 model = glm::mat4(1.0f);
+        for (int row = 0; row < nrRows; ++row)
+        {
+            shader.setFloat("metallic", (float)row / (float)nrRows);
+            for (int col = 0; col < nrColumns; ++col)
+            {
                 // we clamp the roughness to 0.05 - 1.0 as perfectly smooth surfaces (roughness of 0.0) tend to look a bit off
                 // on direct lighting.
-				shader.setFloat("roughness", glm::clamp((float)col / (float)nrColumsn, 0.05f, 1.0f));
-				model = glm::mat4(1.0f);
-				model = glm::translate(model, glm::vec3(
-					(col - (nrColumsn / 2)) * spacing,
-					(row - (nrRows / 2)) * spacing,
-					0.0f
-				));
-				shader.setMat4("model", model);
-				renderCube();
-			}
-        
+                shader.setFloat("roughness", glm::clamp((float)col / (float)nrColumns, 0.05f, 1.0f));
+
+                model = glm::mat4(1.0f);
+                model = glm::translate(model, glm::vec3(
+                    (col - (nrColumns / 2)) * spacing,
+                    (row - (nrRows / 2)) * spacing,
+                    0.0f
+                ));
+                shader.setMat4("model", model);
+                shader.setMat3("normalMatrix", glm::transpose(glm::inverse(glm::mat3(model))));
+                renderSphere();
+            }
         }
 
-        //render light source
+        // render light source (simply re-render sphere at light positions)
+        // this looks a bit off as we use the same shader, but it'll make their positions obvious and 
+        // keeps the codeprint small.
+        for (unsigned int i = 0; i < sizeof(lightPositions) / sizeof(lightPositions[0]); ++i)
+        {
+            glm::vec3 newPos = lightPositions[i] + glm::vec3(sin(glfwGetTime() * 5.0) * 5.0, 0.0, 0.0);
+            newPos = lightPositions[i];
+            shader.setVec3("lightPositions[" + std::to_string(i) + "]", newPos);
+            shader.setVec3("lightColors[" + std::to_string(i) + "]", lightColors[i]);
 
-		for (unsigned int i = 0; i < sizeof(lightPositions) / sizeof(lightPositions[0]); i++)
-		{
-			glm::vec3 newPos = lightPositions[i] + glm::vec3(sin(glfwGetTime() * 5.0f) * 5.0f, 0.0f, 0.0f);
-			newPos = lightPositions[i];
-			shader.setVec3("lightPositions[" + std::to_string(i) + "]", newPos);
-			shader.setVec3("lightColors[" + std::to_string(i) + "]", lightColors[i]);
-
-			model = glm::mat4(1.0f);
-			model = glm::translate(model, newPos);
-			model = glm::scale(model, glm::vec3(0.5f));
-			shader.setMat4("model", model);
-			shader.setMat3("normalMatrix", glm::mat3(glm::transpose(glm::inverse(model))));
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, newPos);
+            model = glm::scale(model, glm::vec3(0.5f));
+            shader.setMat4("model", model);
+            shader.setMat3("normalMatrix", glm::transpose(glm::inverse(glm::mat3(model))));
             renderSphere();
-		}
-
-
+        }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
